532a24ddb9cd7fc07aaf397ff78e66bd8c4d15e9
johnnyclapham <jmclapham@email.wm.edu>
2021-11-10 13:40:46 -0500

    map displays at sample location

--- app/src/main/java/com/example/anaximander/MapsActivity.java
+++ app/src/main/java/com/example/anaximander/MapsActivity.java
@@ -30,6 +30,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mapFragment.getMapAsync(this);
     }
 
+
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -42,10 +43,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+        LatLng home = new LatLng(37.271473, -76.710838);
+        mMap.addMarker(new MarkerOptions().position(home).title("Chancellors Hall"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(home));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(home, 17.0f));
+        mMap.setMapType(2); //https://developers.google.com/android/reference/com/google/android/gms/maps/GoogleMap?hl=en#setMapType(int)
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+//        // Add a marker in Sydney and move the camera
+//        LatLng sydney = new LatLng(-34, 151);
+//        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+//        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
